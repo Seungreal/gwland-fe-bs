21a2315a96ea957fc95e87fef7990604fdc34ad1
suho <suho@suhoui-MacBookAir.local>
2021-03-04 18:33:23 +0900

    proxy set complete

--- src/cli/pages/ClientIndex.jsx
+++ src/cli/pages/ClientIndex.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { makeStyles } from '@material-ui/styles'
 
-export const ClientIndex = ()=>{
-    return <div className="content-body">
-      <center>
+const useStyle = makeStyles(() => ({}))
+
+
+
+export const ClientIndex = () => {
+  const [client, setClient] = useState([])
+  const URL = '/client/all'
+  useEffect(() => {
+    axios.get(URL,)
+      .then((resp) => {
+        alert('restful success')
+        setClient(resp.data)
+      })
+      .catch((err) => {
+        alert('실패')
+        throw err;
+      })
+  }, [])
+
+  return <><div className="content-body">
+    <center>
       <h2>고객관리</h2>
-      <input type="submit" value="고객정보 보기" className="cws-button alt"/>
-      <input type="submit" value="고객정보 입력" className="cws-button alt"/>
-      </center>
-    <div className="container page">
-      <div className="row">
-        <div className="col-md-6">
-        </div>
-      </div>
-    </div>
+      <input type="submit" value="고객정보 보기" className="cws-button alt" />
+      <input type="submit" value="고객정보 입력" className="cws-button alt" />
+    </center>
+    <ul>
+     <center>
+    {client.map(i=>(
+      <li key = {i.cliNum}>
+        {i.cliName}
+      </li>
+    ))}
+    </center>
+    </ul>
   </div>
+  </>
 }
