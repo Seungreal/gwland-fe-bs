d95956696653ce2f9258f3c875eb4c89ffabe72f
SUHOPARK1 <qkrtnghsla@naver.com>
2021-03-25 14:03:46 +0900

    delete버튼 포인터 수정, axios delete 구현, 정말삭제 확인창 기능추가

--- src/webapp/usr/pages/UserList.jsx
+++ src/webapp/usr/pages/UserList.jsx
@@ -7,22 +7,46 @@ import { SvgIcon, Table, TableBody, TableCell, TableHead, TableRow } from '@mate
 export default () => {
   const usr = ['번호', '권한', '플랫폼', '소셜 ID', '고객성함', '이메일주소', '연령대', '성별', '가입날짜']
   const [userList, setUserList] = useState([])
-  const URL = '/user/all'
+  const listURL = '/user/all'
+  const deleteURL = '/user/delete'
+
   useEffect(() => {
-    axios.get(URL,)
+    axios.get(listURL,)
       .then((response) => {
         console.log(response.data)
         setUserList(response.data)
       })
       .catch(err => { alert(err) })
   }, [])
+
+  const deleteUser = ((user) => {
+  const deleteConfirm = window.confirm('정말 삭제하시겠습니까?')
+    if (deleteConfirm == true) {
+      axios.delete(deleteURL, {
+        data: { num: user.num }
+      })
+        .then((response) => {
+          console.log(response.data)
+          alert(`${user.username} 고객님을 삭제하셨습니다.`)
+          window.location.reload('/user/delete')
+        })
+        .catch(err => { alert(err) })
+    } else if (deleteConfirm != true) {
+      alert('취소하셨습니다.')
+      window.location.reload('/user/delete')
+    }
+  })
+
   return <>
     <div className={styles.wrap}>
       <div className={styles.content}>
         <div className={styles.card}>
           <div className={styles.cardHeader}>
-            <h2>고객 정보</h2>
-            <h3>구글은 성별과 나이대를 수집할수 없습니다.</h3>
+            <center>
+              <h2>고객 정보</h2>
+              <h3>※ 구글은 성별과 연령대를 수집할수 없습니다. (구글 정책사항)</h3>
+            </center>
+
           </div>
           <div className={styles.cardBody}>
             <Table>
@@ -46,7 +70,7 @@ export default () => {
                       <TableCell>{user.age}</TableCell>
                       <TableCell>{user.gender}</TableCell>
                       <TableCell>{user.createDate}</TableCell>
-                      <TableCell onClick={() => this.deleteUser(user.id)}>  <DeleteIcon /></TableCell>
+                      <TableCell onClick={() => deleteUser(user)}> <DeleteIcon /></TableCell>
                     </TableRow>
                   )
                 })}
